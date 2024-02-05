97f7ad431550a9a847b36e42dec38a98cf930976
jackiezzz24 <jiaqi.zhao24@gmail.com>
2024-02-05 00:13:17 -0800

    update homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./pages/components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 
-class App extends Component {
-
-  state = {};
-
-  componentDidMount() {
-    setInterval(this.hello, 1000);
-}
-
-
- hello = () => {
-  fetch('/api/hello')
-  .then( res => res.text())
-  .then(res => { this.setState( {message: res})
-  });
-};
-    
- 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.message}</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home/>}></Route>
+          <Route path="/register" exact element={<SignUp/>}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/components/Hero.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "./Button";
+import "../../App.css";
+import "../components/Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero-container">
+      <h1>
+        Scan, Track, Thriv: <br />
+        Your Daily Expenses, Simplified.
+      </h1>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Create an Account
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
