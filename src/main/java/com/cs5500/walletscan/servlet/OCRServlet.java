4ed613bd574734adcd59e52f981067743019bf10
ziyang-666 <songz5@miamioh.edu>
2024-02-06 23:42:17 -0800

    implment OCR servlet

--- src/main/java/com/cs5500/walletscan/servlet/OCRServlet.java
+++ src/main/java/com/cs5500/walletscan/servlet/OCRServlet.java
@@ -1,12 +1,16 @@
 package com.cs5500.walletscan.servlet;
 
+import com.cs5500.walletscan.Utils.WallestScanUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 
 @WebServlet(name="OCRServlet", urlPatterns = {"/ocr"})
 public class OCRServlet extends HttpServlet {
@@ -17,6 +21,15 @@ public class OCRServlet extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        OCRClient client = new OCRClient();
+        // In a class method
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/testImage/receipt.jpg");
+            File file = new File(getClass().getResource("/testImage/receipt.jpg").toURI());
+            String base64String = WallestScanUtils.encodeImageToBase64String(file);
+            client.imageScan(base64String, resp);
+        } catch (URISyntaxException e) {
+            throw new OCRException("Failed to read file");
+        }
     }
 }
