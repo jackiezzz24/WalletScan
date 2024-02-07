07ff934c6ce074cbaa19efcb36a116a5548a2d12
ziyang-666 <songz5@miamioh.edu>
2024-02-07 03:04:59 -0800

    build a new model Text and apply connection and request with apache

--- src/main/java/com/cs5500/walletscan/servlet/OCRClient.java
+++ src/main/java/com/cs5500/walletscan/servlet/OCRClient.java
@@ -1,25 +1,34 @@
 package com.cs5500.walletscan.servlet;
 
+import com.cs5500.walletscan.model.Text;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonReader;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 public class OCRClient {
     
@@ -30,22 +39,45 @@ public class OCRClient {
         return API_URL + API_KEY;
     }
     
-    public void imageScan(String base64Image, HttpServletResponse response) throws OCRException, MalformedURLException {
-        URL url = new URL(buildURL());
-        HttpURLConnection connection;
+    public String performOCR(String base64Image) throws OCRException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(buildURL());
+        httpPost.setHeader("Content-Type", "application/json");
         try {
-            connection = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            throw new OCRException("Failed to connect to internet");
+            httpPost.setEntity(new StringEntity(createJsonRequestBody(base64Image)));
+        } catch (UnsupportedEncodingException e) {
+            throw new OCRException("Failed to build entity");
         }
+        try (CloseableHttpResponse response = client.execute(httpPost)) {
+            String responseString = EntityUtils.toString(response.getEntity());
+            try (JsonReader reader = Json.createReader(new StringReader(responseString))) {
+                JsonObject jsonResponse = reader.readObject();
+                JsonArray responses = jsonResponse.getJsonArray("responses");
+                if (responses != null && !responses.isEmpty()) {
+                    JsonObject responseObj = responses.getJsonObject(0);
+                    return responseObj.getJsonArray("textAnnotations").toString();
+                }
+            }
+        } catch (Exception e) {
+            throw new OCRException("Failed to get response");
+        }
+        return null;
+    }
+    
+    public List<Text> getText(String data) throws OCRException {
+        if (data == null || data.length() == 0) {
+            throw new OCRException("Failed to read data");
+        }
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-        } catch (ProtocolException e) {
-            throw new OCRException("Failed to set request method");
+            return Arrays.asList(mapper.readValue(data, Text[].class));
+        } catch (JsonProcessingException e) {
+            throw new OCRException("Failed to cast json to Text");
         }
-        // Create the JSON request body using the base64 encoded image
+    }
+    
+    private String createJsonRequestBody(String base64Image) {
+        // Create the JSON Object for the request body
         JsonObject jsonRequest = Json.createObjectBuilder()
             .add("requests", Json.createArrayBuilder()
                 .add(Json.createObjectBuilder()
@@ -53,31 +85,13 @@ public class OCRClient {
                         .add("content", base64Image))
                     .add("features", Json.createArrayBuilder()
                         .add(Json.createObjectBuilder()
-                            .add("type", "TEXT_DETECTION"))))
+                            .add("type", "TEXT_DETECTION")
+                        )
+                    )
+                )
             ).build();
-        // Write the JSON request body
-        try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-            wr.write(jsonRequest.toString().getBytes());
-        } catch (IOException e) {
-            throw new OCRException("Failed to read and write output from stream");
-        }
-        
-        // Read the response
-        StringBuilder responseString = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                responseString.append(line);
-            }
-        } catch (IOException e) {
-            throw new OCRException("Failed to read response");
-        }
-        // Output the response for demonstration
-        response.setContentType("application/json");
-        try {
-            response.getWriter().println(responseString.toString());
-        } catch (IOException e) {
-            throw new OCRException("Failed to write response");
-        }
+    
+        // Convert the JSON Object to String
+        return jsonRequest.toString();
     }
 }
--- src/main/java/com/cs5500/walletscan/servlet/OCRServlet.java
+++ src/main/java/com/cs5500/walletscan/servlet/OCRServlet.java
@@ -1,6 +1,7 @@
 package com.cs5500.walletscan.servlet;
 
 import com.cs5500.walletscan.Utils.WallestScanUtils;
+import com.cs5500.walletscan.model.Text;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @WebServlet(name="OCRServlet", urlPatterns = {"/ocr"})
 public class OCRServlet extends HttpServlet {
@@ -22,12 +24,12 @@ public class OCRServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         OCRClient client = new OCRClient();
-        // In a class method
+        List<Text> texts;
         try {
             InputStream inputStream = getClass().getResourceAsStream("/testImage/receipt.jpg");
             File file = new File(getClass().getResource("/testImage/receipt.jpg").toURI());
             String base64String = WallestScanUtils.encodeImageToBase64String(file);
-            client.imageScan(base64String, resp);
+            texts = client.getText(client.performOCR(base64String));
         } catch (URISyntaxException e) {
             throw new OCRException("Failed to read file");
         }
