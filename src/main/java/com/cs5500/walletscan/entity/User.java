f4c97a3b020d6e3cd90b96b4dfc8937c0e139268
jackiezzz24 <jiaqi.zhao24@gmail.com>
2024-02-08 23:06:57 -0800

    update class

---
+++ src/main/java/com/cs5500/walletscan/entity/User.java
@@ -0,0 +1,20 @@
+package com.cs5500.walletscan.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column (name = "email")
+    private String email;
+}
